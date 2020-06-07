8da48cf843687d350f9c5301aad90d545e7590b4
emtwnty <muizahsanu@gmail.com>
2020-06-07 18:06:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/schemaker/AlarmReceiver.kt
@@ -0,0 +1,41 @@
+package com.example.schemaker
+
+import android.app.*
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+
+class AlarmReceiver : BroadcastReceiver() {
+    private lateinit var notificationManager : NotificationManager
+    private lateinit var notificationChannel: NotificationChannel
+    private lateinit var builder : Notification.Builder
+    private val channelId = "i.apps.notifications"
+    private val description = "Text Notification"
+    override fun onReceive(context: Context?, intent: Intent?) {
+        notificationManager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val wibu = intent?.getStringExtra("title")
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = NotificationChannel(
+                channelId,description, NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.GREEN
+            notificationChannel.enableVibration(false)
+            notificationManager.createNotificationChannel(notificationChannel)
+
+            builder = Notification.Builder(context,channelId)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle(wibu)
+                .setContentText("bangsat")
+        }else{
+            builder = Notification.Builder(context)
+                .setContentTitle("halo")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentText("Bangsat")
+        }
+        notificationManager.notify(1234,builder.build())
+    }
+}
